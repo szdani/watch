792d3970806fddbdcbd8e8fe04f30c9ce91a070f
szdani <sz.daniel.attila@gmail.com>
2021-04-10 16:46:06 +0200

    3D Model

---
+++ 3dmodel/README.md
@@ -0,0 +1,21 @@
+
+# 3D Model
+The plan is created with `CADQuery 2.1` in `CQ-editor 0.3.0.dev0`.
+
+### Install micromamba
+Tested with Microbamba 0.9.2
+```Bash
+cd ~/.local
+wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+
+./bin/micromamba shell init -s bash
+source ~/.bashrc
+```
+### Create environment
+```bash
+micromamba create -n cad python=3.9 -c conda-forge
+micromamba activate cad
+
+micromamba install -c cadquery -c conda-forge cadquery=master
+micromamba install -c cadquery -c conda-forge cq-editor=master
+```
---
+++ 3dmodel/watch.py
@@ -0,0 +1,80 @@
+import cadquery as cq
+
+WALL = 1.5
+INNER_RADIUS = 20
+PCB_THICKNESS = 1.6
+BATTERY_HOLDER_HEIGHT = 4.5
+BATTERY_HOLDER_POSITION_1 = (17.5, 0)
+BATTERY_HOLDER_POSITION_2 = (-17.5, 0)
+
+### Watch Face ###
+body = cq.Workplane("XY" )\
+        .circle(INNER_RADIUS).extrude(6)\
+        .faces("+Z").shell(WALL, kind='arc')\
+        .cut(cq.Workplane("XY").circle(1.5).extrude(10)\
+        .rotate((0, 0, 0),(-19, 10.5, 0), 90)\
+        .translate((10-3.5,19-3,3.7)))
+
+
+battery_holder_left = cq.Workplane("XY" )\
+        .box(20,1.5,BATTERY_HOLDER_HEIGHT)\
+        .translate((0,-11.0,2))
+battery_holder_right = battery_holder_left.mirror(mirrorPlane="XZ")
+
+watchface_holder_1 = cq.Workplane("XY" )\
+        .circle(0.8)\
+        .extrude(BATTERY_HOLDER_HEIGHT+PCB_THICKNESS)\
+        .translate(BATTERY_HOLDER_POSITION_1)
+watchface_holder_2 = watchface_holder_1.mirror(mirrorPlane="YZ")
+
+### LEGS ###
+leg_base = cq.Workplane("XY")\
+        .line(26.5, 0)\
+        .line(0, 5)\
+        .line(-26.5, 4)\
+        .line(-26.5, -4)\
+        .line(0, -5)\
+        .close().extrude(6)
+cut1 = cq.Workplane("XY").circle(80).extrude(10).translate((0,-76.5,-10))\
+             .rotate((0,0,0),(1,0,0),90)
+cut2 = cq.Workplane("XY").circle(80).extrude(10).translate((0,-71.75,-10))\
+             .rotate((0,0,0),(1,0,0),90)
+cut3_watch_base = cq.Workplane("XY").circle(WALL+INNER_RADIUS).extrude(10)
+cut4 = cq.Workplane("XY").circle(1).extrude(30)\
+                .translate((24,0.6,-15))\
+                .rotate((0, 0, 0),(1, 0, 0), 90)
+cut5 = cut4.mirror(mirrorPlane="YZ")
+
+right_leg = leg_base\
+        .cut(cut1)\
+        .cut(leg_base.cut(cut2))\
+            .edges("|Y").fillet(1.5)\
+        .translate((0,11,-1.5))\
+        .cut(cut3_watch_base)\
+        .cut(cut4).cut(cut5)
+        
+left_leg = right_leg.mirror(mirrorPlane="XZ")
+
+result = body\
+            .union(right_leg)\
+            .union(left_leg)\
+            .union(battery_holder_left)\
+            .union(battery_holder_right)\
+            .union(watchface_holder_1)\
+            .union(watchface_holder_2)
+
+top_body = cq.Workplane("XY")\
+    .polyline([(0,INNER_RADIUS),
+               (0,INNER_RADIUS+WALL),
+               (2.5,INNER_RADIUS+WALL),
+               (2.5+WALL,INNER_RADIUS),
+               (2.5+WALL,INNER_RADIUS-2),
+               (2.5,INNER_RADIUS-2),
+               (2.5,INNER_RADIUS)])\
+    .close()\
+    .revolve(axisStart=(1, 0), axisEnd=(0, 0), angleDegrees=360)\
+    .rotate((0, 0, 0),(0, 1, 0), 90).translate((-50,0,4))
+
+
+show_object(result)
+show_object(top_body)
