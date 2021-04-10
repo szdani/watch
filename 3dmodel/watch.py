bcc263b6dffd1ff83e1c3e0a3f5ec00032bec337
szdani <sz.daniel.attila@gmail.com>
2021-04-10 17:51:23 +0200

    3d model with top-bottom connection

--- 3dmodel/watch.py
+++ 3dmodel/watch.py
@@ -1,20 +1,27 @@
 import cadquery as cq
 
 WALL = 1.5
-INNER_RADIUS = 20
+INNER_RADIUS = 21
 PCB_THICKNESS = 1.6
 BATTERY_HOLDER_HEIGHT = 4.5
 BATTERY_HOLDER_POSITION_1 = (17.5, 0)
 BATTERY_HOLDER_POSITION_2 = (-17.5, 0)
+BOTTOM_PART_HEIGHT = 6
 
 ### Watch Face ###
 body = cq.Workplane("XY" )\
-        .circle(INNER_RADIUS).extrude(6)\
+        .circle(INNER_RADIUS).extrude(BOTTOM_PART_HEIGHT)\
         .faces("+Z").shell(WALL, kind='arc')\
         .cut(cq.Workplane("XY").circle(1.5).extrude(10)\
-        .rotate((0, 0, 0),(-19, 10.5, 0), 90)\
-        .translate((10-3.5,19-3,3.7)))
+            .rotate((0, 0, 0),(-19, 10.5, 0), 90)\
+            .translate((10-3.5,19-3,3.7)))
 
+connection_shell = cq.Workplane("XY" )\
+        .circle(INNER_RADIUS).extrude(BOTTOM_PART_HEIGHT+1.5)\
+        .faces("+Z").shell(0.8, kind='arc')\
+        .cut(cq.Workplane("XY").circle(1.5).extrude(10)\
+            .rotate((0, 0, 0),(-19, 10.5, 0), 90)\
+            .translate((10-3.5,19-3,3.7)))
 
 battery_holder_left = cq.Workplane("XY" )\
         .box(20,1.5,BATTERY_HOLDER_HEIGHT)\
@@ -29,10 +36,10 @@ watchface_holder_2 = watchface_holder_1.mirror(mirrorPlane="YZ")
 
 ### LEGS ###
 leg_base = cq.Workplane("XY")\
-        .line(26.5, 0)\
+        .line(28.5, 0)\
         .line(0, 5)\
-        .line(-26.5, 4)\
-        .line(-26.5, -4)\
+        .line(-28.5, 4)\
+        .line(-28.5, -4)\
         .line(0, -5)\
         .close().extrude(6)
 cut1 = cq.Workplane("XY").circle(80).extrude(10).translate((0,-76.5,-10))\
@@ -41,7 +48,7 @@ cut2 = cq.Workplane("XY").circle(80).extrude(10).translate((0,-71.75,-10))\
              .rotate((0,0,0),(1,0,0),90)
 cut3_watch_base = cq.Workplane("XY").circle(WALL+INNER_RADIUS).extrude(10)
 cut4 = cq.Workplane("XY").circle(1).extrude(30)\
-                .translate((24,0.6,-15))\
+                .translate((26,0.4,-15))\
                 .rotate((0, 0, 0),(1, 0, 0), 90)
 cut5 = cut4.mirror(mirrorPlane="YZ")
 
@@ -56,6 +63,7 @@ right_leg = leg_base\
 left_leg = right_leg.mirror(mirrorPlane="XZ")
 
 result = body\
+            .union(connection_shell)\
             .union(right_leg)\
             .union(left_leg)\
             .union(battery_holder_left)\
